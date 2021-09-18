8ae2bb5156ce146b0e302206b695d36216ad9430
hung6033 <90978454+hung6033@users.noreply.github.com>
2021-09-19 00:57:04 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Tặng Hà !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Ahihi',
+    title: 'Dù sau này ai làm em buồn khó khăn ko biết giải quyết lẻ loi 1 mình thì đừng buồn 😉',
+    desc: 'Thì nhớ là anh vẫn sẽ luôn bên em an ủn và giúp em vượt qua khó khăn mong em luôn vui vẻ sống hạnh phúc 😊 anh xin hứa danh dự',
+    btnYes: 'Em cảm ơn nhiều 😊',
+    btnNo: 'Rất ghét anh Thành 😢',
+    question:'Và sau này dù có sao thì đừng có ghét hay bơ anh Thành nhé',
+    btnReply: 'Gửi cho anh :>',
+    reply: 'Nhắn câu trả lời vào messenger của anh nhé :))',
+    mess: 'Chúc em luôn vui vẻ thi được điểm cao 😊',
+    messDesc: 'Mong Wed giúp e giải trí thi tốt hơn 😁',
+    btnAccept: 'Em cám ơn anh cũng thế :))',
+    messLink: 'https://www.facebook.com/profile.php?id=100010045690226'
+}
